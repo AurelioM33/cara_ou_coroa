1af56a8af978e4a32086e9e84d42a21815861562
AurelioM33 <m.aurelio887@gmail.com>
2022-05-05 21:58:17 -0300

    first commit

---
+++ lib/Resultado.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatefulWidget {
+  String valor;
+
+  Resultado(this.valor);
+
+  @override
+  State<Resultado> createState() => _ResultadoState();
+}
+
+class _ResultadoState extends State<Resultado> {
+  @override
+  Widget build(BuildContext context) {
+    var caminhoImages;
+
+    if (widget.valor == "cara") {
+      caminhoImages = "images/moeda_cara.png";
+    } else {
+      caminhoImages = "images/moeda_coroa.png";
+    }
+
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset(caminhoImages),
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Image.asset("images/botao_voltar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
